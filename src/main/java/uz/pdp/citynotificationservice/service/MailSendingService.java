3ac6dd10a2880b2142cd58aea43b81512034edfc
Azamat <hasanjonovazamat@gmail.com>
2023-08-04 10:28:55 +0500

    Minor improvements

--- src/main/java/uz/pdp/citynotificationservice/service/MailSendingService.java
+++ src/main/java/uz/pdp/citynotificationservice/service/MailSendingService.java
@@ -7,19 +7,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import uz.pdp.citynotificationservice.dto.MailDto;
 
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class MailSendingService {
-    private final Random random = new Random(10000);
     private final JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
     private String sender;
 
     public String sendMessage(MailDto mailDto) {
-        Long l = (long) random.nextInt(10000);
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject("Verification");
         simpleMailMessage.setTo(mailDto.getEmail());
         simpleMailMessage.setFrom(sender);
         simpleMailMessage.setText(mailDto.getMessage());
